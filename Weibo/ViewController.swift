ce9fd9acf9b33093fd4b62241a802bff59f48e0f
dadai <macache@163.com>
2017-01-08 00:44:35 +0800

    Upload Initial Project
    上传初始Project，创建子文件夹，App Icon，启动图片等，修改Bundle display Name

---
+++ Weibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weibo
+//
+//  Created by daixianglong on 2017/1/8.
+//  Copyright © 2017年 Dale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
